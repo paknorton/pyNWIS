313a4dff19d324264f3f6f3d47af7f539d0ecddd
Parker Norton <pnorton@usgs.gov>
2021-06-23 12:08:56 -0600

    Pulls from daily values REST

--- pyNWIS/utilities/nwis_daily_rest.py
+++ pyNWIS/utilities/nwis_daily_rest.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 """This script downloads daily streamflow observations from
 the NWIS REST service for a set of streamgages.
 Two data files are output
@@ -20,168 +20,197 @@ from time import strftime
 import argparse
 import logging
 
-# The urllib library is named differently for Python 2 and 3
-try:
-    # For Python 3.0 and later
-    from urllib.request import urlopen
-except ImportError:
-    # Fall back to Python 2's urllib2
-    from urllib2 import urlopen
+from collections import OrderedDict
+from urllib.request import urlopen   # , Request
+# from urllib.error import HTTPError
 
-__version__ = '0.1'
+__version__ = '0.3'
 
-# Command line arguments
-parser = argparse.ArgumentParser(description='Download streamflow observations from NWIS REST service.')
-parser.add_argument('outfile', help='Output filename base (e.g. nwis.tab)')
-# parser.add_argument('-w', '--wateryears', help='Observations are stored by water years', action='store_true')
-# parser.add_argument('-d', '--daterange',
-#                     help='Starting and ending date (YYYY-MM-DD YYYY-MM-DD)',
-#                     nargs=2, metavar=('startDate','endDate'), required=True)
-parser.add_argument('-t', '--statRepType', help='Statistic report type',
-                    choices=['annual', 'monthly', 'daily'], default='annual')
-parser.add_argument('-s', '--stat', help='Type of statistic', choices=['mean'], default='mean')
-parser.add_argument('-O', '--overwrite', help='Overwrite existing output file', action='store_true')
-parser.add_argument('-R', '--region', help='Hydrologic Unit Code for stations to select')
 
-args = parser.parse_args()
+# stat_choices = ['00001', '00002', '00003', '00008', '00009', '00010', '00013']
 
-# List of streamgages for download
-sglist = ['05536890', '05536995', '05537980', '05527500', '05543010', '05543500']
-sitelist = ','.join(sglist)
 
-# List of parameter codes to retrieve
-plist = ['00095', '00300', '00400', '63680', '99133']
-paramList = ','.join(plist)
+def main():
+    # Command line arguments
+    parser = argparse.ArgumentParser(description='Download streamflow observations from NWIS REST service.')
+    parser.add_argument('outfile', help='Output filename base (e.g. nwis.tab)')
+    parser.add_argument('-d', '--daterange',
+                        help='Starting and ending date (YYYY-MM-DD YYYY-MM-DD)',
+                        nargs=2, metavar=('startDate', 'endDate'), required=True)
+    parser.add_argument('-O', '--overwrite', help='Overwrite existing output file', action='store_true')
+    parser.add_argument('-P', '--parameters', help='Space separated list of parameter codes', nargs='+',
+                        default=['00060'], type=str)
+    parser.add_argument('-R', '--region', help='Hydrologic Unit Code for stations to select',
+                        default=None, type=str)
+    parser.add_argument('-s', '--stat', help='List of statistics', nargs='+', default=['00003'])
+    parser.add_argument('-S', '--sites', help='Space separated list of streamgages', nargs='+',
+                        default=None, type=str)
+    parser.add_argument('--show_restricted', help='Retrieved parameters/values that have access restrictions',
+                        action='store_true')
 
-# Additional parts to add to output filenames
-addin = '{0:s}_region_{1:s}'.format(args.statRepType, args.region)
+    args = parser.parse_args()
 
-# Specifying observations by water year is only legal for annual report types
-if args.statRepType == 'annual' and args.wateryears:
-    statYearType = '&statYearType=water'
-    addin += '_WY'
-else:
-    statYearType = ''
+    if args.region is not None:
+        args.sites = None
 
-# Construct the filenames for streamgage observations, streamgage information, and the log file
-dirpart = os.path.dirname(args.outfile)
-nameparts = os.path.splitext(os.path.basename(args.outfile))
-obsfile = os.path.join(dirpart, '{0:s}_{1:s}_obs{2:s}'.format(nameparts[0], addin, nameparts[1]))
-stnfile = os.path.join(dirpart, '{0:s}_{1:s}_stn{2:s}'.format(nameparts[0], addin, nameparts[1]))
-logfile = os.path.join(dirpart, '{0:s}_{1:s}.log'.format(nameparts[0], addin))
+    # Additional parts to add to output filenames
+    addin = ''
+    if args.region is not None:
+        addin = f'_region_{args.region}'
 
-print('Streamgage observation file: {0:s}'.format(obsfile))
-print('Streamgage information file: {0:s}'.format(stnfile))
-print('Session log file: {0:s}'.format(logfile))
+    # Construct the filenames for streamgage observations, streamgage information, and the log file
+    dirpart = os.path.dirname(args.outfile)
+    nameparts = os.path.splitext(os.path.basename(args.outfile))
+    obsfile = os.path.join(dirpart, f'{nameparts[0]}{addin}_obs{nameparts[1]}')
+    stnfile = os.path.join(dirpart, f'{nameparts[0]}{addin}_stn{nameparts[1]}')
+    logfile = os.path.join(dirpart, f'{nameparts[0]}{addin}.log')
 
-if not args.overwrite and os.path.isfile(stnfile):
-    print(
-        "The streamflow information file, {0:s}, already exists.\nTo force overwrite specify -O on command line".format(
-            stnfile))
-    exit(1)
+    print(f'Streamgage observation file: {obsfile}')
+    print(f'Streamgage information file: {stnfile}')
+    print(f'Session log file: {logfile}')
 
-if not args.overwrite and os.path.isfile(obsfile):
-    print(
-        "The streamflow observation file, {0:s}, already exists.\nTo force overwrite specify -O on command line".format(
-            obsfile))
-    exit(1)
+    if not args.overwrite and os.path.isfile(stnfile):
+        print(f'The streamflow information file, {stnfile}, already exists.\nTo force overwrite specify -O on command line')
+        exit(1)
 
-# Open logfile and start collecting basic information to write out later
-logging.basicConfig(filename=logfile, level=logging.INFO,
-                    format='%(levelname)s:%(asctime)s:%(message)s')
+    if not args.overwrite and os.path.isfile(obsfile):
+        print(f'The streamflow observation file, {obsfile}, already exists.\nTo force overwrite specify -O on command line')
+        exit(1)
 
-logging.info('Program executed {0:s}'.format(strftime('%Y-%m-%d %H:%M:%S %z')))
-logging.info(" ".join(sys.argv))
-logging.info('Script version: {0:s}'.format(__version__))
-logging.info('Script directory: {0:s}'.format(os.path.dirname(os.path.abspath(__file__))))
-logging.info('Python: {0:s} ({1:s})'.format(platform.python_implementation(), platform.python_version()))
-logging.info('Host: {0:s}'.format(platform.node()))
-logging.info('-'*70)
-logging.info('Current directory: {0:s}'.format(os.getcwd()))
-logging.info(' Observation file: {0:s}'.format(obsfile))
-logging.info('Station info file: {0:s}'.format(stnfile))
-logging.info('         Log file: {0:s}'.format(logfile))
+    # Open logfile and start collecting basic information to write out later
+    logging.basicConfig(filename=logfile, level=logging.INFO,
+                        format='%(levelname)s:%(asctime)s:%(message)s')
 
-# URLs can be generated/tested at: http://waterservices.usgs.gov/rest/Site-Test-Tool.html
-base_url = 'http://waterservices.usgs.gov/nwis'
+    logging.info(f'Program executed {strftime("%Y-%m-%d %H:%M:%S %z")}')
+    logging.info(" ".join(sys.argv))
+    logging.info(f'Script version: {__version__}')
+    logging.info(f'Script directory: {os.path.dirname(os.path.abspath(__file__))}')
+    logging.info(f'Python: {platform.python_implementation()} ({platform.python_version()})')
+    logging.info(f'Host: {platform.node()}')
+    logging.info('-'*70)
+    logging.info(f'Current directory: {os.getcwd()}')
+    logging.info(f' Observation file: {obsfile}')
+    logging.info(f'Station info file: {stnfile}')
+    logging.info(f'         Log file: {logfile}')
 
-stn_url = '{0:s}/site/?format=rdb&sites={1:s}&siteOutput=expanded&siteStatus=active&parameterCd={2:s}&siteType=ST' \
-    .format(base_url, sitelist, paramList)
+    # URLs can be generated/tested at: http://waterservices.usgs.gov/rest/Site-Test-Tool.html
+    base_url = 'https://waterservices.usgs.gov/nwis'
 
-logging.info('Water years: {0:s}'.format(args.statRepType == 'annual' and args.wateryears))
-logging.info('Sites: {0:s}'.format(sitelist))
-logging.info('Report type: {0:s}'.format(args.statRepType))
-logging.info('Statistic type: {0:s}'.format(args.stat))
-logging.info('-'*70)
-logging.info('Base URL: {0:s}'.format(base_url))
-logging.info('Station URL: {0:s}'.format(stn_url))
+    stn_pieces = OrderedDict()
+    stn_pieces['format'] = 'rdb'
 
-# Open NWIS REST site service
-streamgagesPage = urlopen(stn_url)
+    if args.region is not None:
+        stn_pieces['huc'] = args.region
+    else:
+        stn_pieces['sites'] = ','.join([ii for ii in args.sites])
 
-# Open station and observation files
-stn_hdl = open(stnfile, "w")
-obs_hdl = open(obsfile, "w")
+    stn_pieces['siteOutput'] = 'expanded'
+    stn_pieces['siteStatus'] = 'all'
+    stn_pieces['parameterCd'] = ','.join([ii for ii in args.parameters])
+    stn_pieces['siteType'] = 'ST'
+    stn_final = '&'.join([f'{kk}={vv}' for kk, vv in stn_pieces.items()])
 
-t1 = re.compile('^#.*$\n?', re.MULTILINE)   # remove comment lines
-t2 = re.compile('^5s.*$\n?', re.MULTILINE)  # remove field length lines
+    stn_url = f'{base_url}/site/?{stn_final}'
 
-# Get the list of streamgages within the specified region
-streamgagesFromREST = streamgagesPage.read()
+    logging.info(f'Region: {args.region}')
+    logging.info(f'Sites: {args.sites}')
+    # logging.info(f'Report type: {args.statRepType}')
+    logging.info(f'Parameters: {args.parameters}')
+    logging.info(f'Statistic type: {args.stat}')
+    logging.info('-'*70)
+    logging.info(f'Base URL: {base_url}')
+    logging.info(f'Station URL: {stn_url}')
 
-# Strip the comment lines and field length lines from the result
-streamgagesFromREST = t1.sub('', streamgagesFromREST, 0)
-streamgagesFromREST = t2.sub('', streamgagesFromREST, 0)
+    # Open station and observation files
+    stn_hdl = open(stnfile, "w")
+    obs_hdl = open(obsfile, "w")
 
-fld = {}
-print(streamgagesFromREST)
-exit()
+    t1 = re.compile('^#.*$\n?', re.MULTILINE)   # remove comment lines
+    t2 = re.compile('^5s.*$\n?', re.MULTILINE)  # remove field length lines
 
-# Each request gives a new header; we only want one
-header_written = False
+    # Retrieve stations from NWIS site service
+    response = urlopen(stn_url)
+    encoding = response.info().get_param('charset', failobj='utf8')
+    streamgage_site_page = response.read().decode(encoding)
 
-logging.info('========== Streamgage observation URLs ==========')
-# Loop through each site and download the observations
-for cStreamgage in streamgagesFromREST.split('\n'):
-    if len(cStreamgage) > 0:
-        ff = cStreamgage.split('\t')
-        if ff[0] == 'agency_cd':
-            # Get the fieldnames for the site information
-            cc = 0
-            for sf in ff:
-                # Build a list of indices to each field name
-                fld[sf] = cc
-                cc += 1
+    # Strip the comment lines and field length lines from the result
+    streamgage_site_page = t1.sub('', streamgage_site_page, 0)
+    streamgage_site_page = t2.sub('', streamgage_site_page, 0)
+
+    # Build the non-changing parts of the REST URL for pulling streamflow values
+    url_pieces = OrderedDict()
+    url_pieces['format'] = 'rdb'
+
+    url_pieces['startDT'] = args.daterange[0]
+    url_pieces['endDT'] = args.daterange[1]
+
+    url_pieces['parameterCd'] = ','.join([ii for ii in args.parameters])  # Discharge
+    # url_pieces['statReportType'] = args.statRepType
+    url_pieces['statCd'] = ','.join([ii for ii in args.stat])
+
+    if args.show_restricted:
+        url_pieces['access'] = 3
+
+    fld = {}
+
+    # Each request gives a new header; we only want one
+    header_written = False
+
+    logging.info('========== Streamgage observation URLs ==========')
+    # Loop through each site and download the observations
+    for cStreamgage in streamgage_site_page.split('\n'):
+        if len(cStreamgage) > 0:
+            ff = cStreamgage.split('\t')
+            if ff[0] == 'agency_cd':
+                # Get the fieldnames for the site information
+                cc = 0
+                for sf in ff:
+                    # Build a list of indices to each field name
+                    fld[sf] = cc
+                    cc += 1
+                stn_hdl.write(cStreamgage + '\n')
+                continue
+
+            url_pieces['site'] = ff[fld['site_no']]
+
+            url_final = '&'.join([f'{kk}={vv}' for kk, vv in url_pieces.items()])
+
+            obs_url = f'{base_url}/dv/?{url_final}'
+
+            logging.info(obs_url)
+            sys.stdout.write(f'\rDownloading observations for streamgage: {ff[fld["site_no"]]}')
+            sys.stdout.flush()
+
+            # Read site data
+            response = urlopen(obs_url)
+            encoding = response.info().get_param('charset', failobj='utf8')
+            streamgage_obs_page = response.read().decode(encoding)
+
+            # Strip the comment lines and field length lines from the result using regex
+            streamgage_obs_page = t1.sub('', streamgage_obs_page, 0)
+            streamgage_obs_page = t2.sub('', streamgage_obs_page, 0)
+
+            # Write the streamgage observations to the output file
+            for obs in streamgage_obs_page.split('\n'):
+                if obs.split('\t')[0] == 'agency_cd':
+                    # We only want a single header in the output file
+                    if header_written:
+                        continue
+                    else:
+                        header_written = True
+                if len(obs) > 0:
+                    if obs[0] != '\t':
+                        # Empty data returns can have all tabs
+                        obs_hdl.write(obs + '\n')
+
+            # Write the streamgage information file
             stn_hdl.write(cStreamgage + '\n')
-            continue
+            sys.stdout.write('\r' + ' '*60 + '\r')
+    stn_hdl.close()
+    obs_hdl.close()
 
-        obs_url = '{0:s}/stat/?format=rdb&site={1:s}&statReportType={2:s}&statType={3:s}{4:s}&parameterCd=00060' \
-            .format(base_url, ff[fld['site_no']], args.statRepType, args.stat, statYearType)
-        logging.info(obs_url)
-        print("Downloading observations for streamgage: {0:s}".format(ff[fld['site_no']]))
+    print(f'Summary written to {logfile}')
 
-        # Read site data
-        streamgageObsPage = urlopen(obs_url)
-        streamgageObservations = streamgageObsPage.read()
 
-        # Strip the comment lines and field length lines from the result using regex
-        streamgageObservations = t1.sub('', streamgageObservations, 0)
-        streamgageObservations = t2.sub('', streamgageObservations, 0)
-
-        # Write the streamgage observations to the output file
-        for obs in streamgageObservations.split('\n'):
-            if obs.split('\t')[0] == 'agency_cd':
-                # We only want a single header in the output file
-                if header_written:
-                    continue
-                else:
-                    header_written = True
-            if len(obs) > 0:
-                obs_hdl.write(obs + '\n')
-
-        # Write the streamgage information file
-        stn_hdl.write(cStreamgage + '\n')
-stn_hdl.close()
-obs_hdl.close()
-
-print("Summary written to {0:s}".format(logfile))
+if __name__ == '__main__':
+    main()
